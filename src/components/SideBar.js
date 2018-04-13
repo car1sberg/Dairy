9346abe4c9801424613d25bd049b4ab6d356bab3
Volodymyr Ersteniuk <deephell.best@gmail.com>
2018-04-13 09:06:04 +0300

    fixing again urls

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
     return (
         <aside className="aside">
-            <h1><Link to="/">dairy app</Link></h1>
+            <h1><Link to="/Dairy/">dairy app</Link></h1>
             <p>Comment with no sense</p>
         </aside>
     )
